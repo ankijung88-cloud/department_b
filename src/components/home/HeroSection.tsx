a5b72aa0733cc13dfde7b7fc3840aac436af9b45
ankijung88 <ankijung88@gmail.com>
2026-02-24 16:12:39 +0900

    hero-bg

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -14,7 +14,7 @@ export const HeroSection: React.FC = () => {
             <div
                 className="absolute inset-0 bg-cover bg-center z-0"
                 style={{
-                    backgroundImage: 'url("/image/hero.png")',
+                    backgroundImage: 'url("/image/hero.jpg")',
                 }}
             >
                 {/* Spotlight effect overlay: lit center, dark edges */}
